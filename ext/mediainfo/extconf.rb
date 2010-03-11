a843a9c87b499ba6f02a221ba786721373b2c9f8
Chris Hoffman <cehoffman@gmail.com>
2010-03-11 10:09:52 -0600

    Get building mediainfo working

--- ext/mediainfo/extconf.rb
+++ ext/mediainfo/extconf.rb
@@ -3,8 +3,20 @@ require "pathname"
 
 root = Pathname.new(__FILE__).dirname.realpath
 
-puts "#{root + "Project/GNU/Library/configure"} --enable-shared --disable-static --libdir=/usr/lib"
-system "#{root + "Project/GNU/Library/configure"} --enable-shared --disable-static --libdir=/usr/lib"
-system "cd #{root + "Project/GNU/Library"} && make"
+Dir.chdir root + "ZenLib/Project/GNU/Library" do
+  system "./configure --libdir=#{root}"
+  system "make install"
+end
 
-create_makefile("mediainfo/mediainfo")
+Dir.chdir root + "MediaInfoLib/Project/GNU/Library" do
+  system "./configure --libdir=#{root}" # --enable-shared --disable-static
+  system "make install"
+end
+
+# Wow mkmf is such a pain to get this working
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] = "g++"
+with_cflags "-I#{root + "ZenLib/Source"} -I#{root + "MediaInfoLib/Source"}" do
+  with_ldflags "-L#{root} -lmediainfo -lzen" do
+    create_makefile("mediainfo/mediainfo")
+  end
+end
