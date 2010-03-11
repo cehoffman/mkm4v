1787e18637488698d8adc8dfe32876d9eca8939b
Chris Hoffman <cehoffman@gmail.com>
2010-03-11 02:49:28 -0600

    Make the cli options testable

--- lib/mkm4v/cli.rb
+++ lib/mkm4v/cli.rb
@@ -1,4 +1,27 @@
+require "trollop"
+
 class Mkm4v::Cli
-  def self.run
+  Opts = Trollop::Parser.new do
+    version "#{$0} Version #{Mkm4v::Version} Copyright (C) 2010 Chris Hoffman"
+    banner <<-EOS.undent
+      Usage: #{$0} [options] -i <filename> [filename ...]
+ \n
+    EOS
+
+    opt :input, "List of files to convert", type: :strings
+  end
+
+  def self.run(*args)
+    Opts.parse args
+  rescue Trollop::CommandlineError => e
+    Opts.educate
+    warn "\nError: #{e.message}"
+    exit 1
+  rescue Trollop::HelpNeeded
+    Opts.educate
+    exit
+  rescue Trollop::VersionNeeded
+    puts Opts.version
+    exit
   end
 end
