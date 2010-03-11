10fd025bcf18d2d483ffe2ce2a21483b14f1130a
Chris Hoffman <cehoffman@gmail.com>
2010-03-11 12:07:51 -0600

    Have the glue to get properties of tracks into ruby space now

--- ext/mediainfo/mediainfo.c
+++ ext/mediainfo/mediainfo.c
@@ -7,6 +7,14 @@ extern "C"
 {
 
 static VALUE rb_cMediaInfo;
+static ID general;
+static ID video;
+static ID audio;
+static ID text;
+static ID chapters;
+static ID image;
+static ID menu;
+static ID max;
 
 static void mediainfo_mark(void *mi) {
 
@@ -51,6 +59,74 @@ static VALUE mediainfo_to_s(VALUE self) {
   return rb_str_new2(MediaInfo_Inform(mi, 0));
 }
 
+static MediaInfo_stream_C get_stream_id(VALUE sym) {
+  MediaInfo_stream_C stream;
+  ID kind = rb_to_id(sym);
+
+  if (kind == general) {
+    stream = MediaInfo_Stream_General;
+  } else if (kind == video) {
+    stream = MediaInfo_Stream_Video;
+  } else if (kind == audio) {
+    stream = MediaInfo_Stream_Audio;
+  } else if (kind == text) {
+    stream = MediaInfo_Stream_Text;
+  } else if (kind == chapters) {
+    stream = MediaInfo_Stream_Chapters;
+  } else if (kind == image) {
+    stream = MediaInfo_Stream_Image;
+  } else if (kind == menu) {
+    stream = MediaInfo_Stream_Menu;
+  } else {
+    stream = MediaInfo_Stream_Max;
+  }
+
+  return stream;
+}
+
+static VALUE mediainfo_tracks(VALUE self, VALUE sym) {
+  void *mi;
+  Data_Get_Struct(self, void, mi);
+
+  return INT2FIX(MediaInfo_Count_Get(mi, get_stream_id(sym), -1));
+}
+
+static VALUE mediainfo_track_info(VALUE self, VALUE sym, VALUE num, VALUE type, VALUE kind) {
+  Check_Type(type, T_STRING);
+
+  void *mi;
+  Data_Get_Struct(self, void, mi);
+
+  VALUE hash = rb_hash_new();
+  ID info = rb_to_id(kind);
+  MediaInfo_info_C info_kind;
+  MediaInfo_stream_C stream = get_stream_id(sym);
+  int stream_id = FIX2INT(num);
+  int pos = MediaInfo_Count_Get(mi, stream, stream_id);
+
+  if (rb_intern("name") == info) {
+    info_kind = MediaInfo_Info_Name;
+  } else if (rb_intern("text") == info) {
+    info_kind = MediaInfo_Info_Text;
+  } else if (rb_intern("measure") == info) {
+    info_kind = MediaInfo_Info_Measure;
+  } else if (rb_intern("options") == info) {
+    info_kind = MediaInfo_Info_Options;
+  } else if (rb_intern("name_text") == info) {
+    info_kind = MediaInfo_Info_Name_Text;
+  } else if (rb_intern("measure_text") == info) {
+    info_kind = MediaInfo_Info_Measure_Text;
+  } else if (rb_intern("info") == info) {
+    info_kind = MediaInfo_Info_Info;
+  } else {
+    info_kind = MediaInfo_Info_HowTo;
+  }
+
+  rb_hash_aset(hash, rb_funcall(rb_funcall(type, rb_intern("downcase"), 0), rb_intern("to_sym"), 0), rb_str_new2(MediaInfo_Get(mi, stream, stream_id, RSTRING_PTR(type), info_kind, (MediaInfo_info_C)0)));
+
+  return hash;
+}
+
 void Init_mediainfo() {
   rb_cMediaInfo = rb_define_class("MediaInfo", rb_cObject);
   rb_define_alloc_func(rb_cMediaInfo, mediainfo_alloc);
@@ -58,6 +134,17 @@ void Init_mediainfo() {
   // The typecast of the functions is necessary because of the C++ compiler
   rb_define_method(rb_cMediaInfo, "initialize", (VALUE (*)(...))mediainfo_init, 1);
   rb_define_method(rb_cMediaInfo, "to_s", (VALUE (*)(...))mediainfo_to_s, 0);
+  rb_define_method(rb_cMediaInfo, "tracks", (VALUE (*)(...))mediainfo_tracks, 1);
+  rb_define_method(rb_cMediaInfo, "track_info", (VALUE (*)(...))mediainfo_track_info, 4);
+
+  // Stream types
+  general = rb_intern("general");
+  video = rb_intern("video");
+  audio = rb_intern("audio");
+  text = rb_intern("text");
+  chapters = rb_intern("chapters");
+  image = rb_intern("image");
+  menu = rb_intern("menu");
 }
 
 }
