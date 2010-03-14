24dc3b4103fcd88cb28ed762abddb4a5dc8eb7de
Chris Hoffman <cehoffman@gmail.com>
2010-03-14 11:53:20 -0500

    Reduce initialization overhead and memory usage

--- lib/mediainfo/track.rb
+++ lib/mediainfo/track.rb
@@ -5,31 +5,33 @@ module MediaInfo::Track
   end
 
   def initialize(source, track)
-    @source, @track, @track_type = source, track, self.class.to_s[/::(\w+)Track$/, 1].downcase.to_sym
+    @source, @track = source, track
 
     self.class.class_variable_get(:@@properties).each do |name, param, converter|
-      instance_variable_set("@#{name}".to_sym, converter.call(info(param)))
+      instance_variable_set(name, converter.call(info(param)))
     end
 
     self.class.class_variable_get(:@@initializations).each { |block| instance_eval &block }
   end
 
   def info(query)
-    @source.track_info @track_type, @track, query
+    @source.track_info self.class.class_variable_get(:@@track_type), @track, query
   end
 
   Integer = ->(raw) { raw.to_i }
   Float = ->(raw) { raw.to_f }
+  String = ->(raw) { raw }
 
   module ClassMethods
     def self.extended(klass)
       klass.class_variable_set(:@@properties, [])
       klass.class_variable_set(:@@initializations, [])
+      klass.class_variable_set(:@@track_type, klass.name[/::(\w+)Track$/, 1].downcase.to_sym)
     end
 
-    def property(name, param, converter = ->(raw) { raw } )
-      attr_reader name
-      class_variable_get(:@@properties) << [name, param, converter]
+    def property(name, param, converter = String )
+      attr_reader name.to_sym
+      class_variable_get(:@@properties) << ["@#{name}".to_sym, param, converter]
     end
 
     def initialized(&block)
