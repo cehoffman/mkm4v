8301fa60899eb753fb5d966e1bb7b1fb72a55892
Chris Hoffman <cehoffman@gmail.com>
2010-03-14 16:12:13 -0500

    Add more properties to general track

--- lib/mediainfo/general.rb
+++ lib/mediainfo/general.rb
@@ -1,8 +1,13 @@
 class MediaInfo::GeneralTrack
   include MediaInfo::Track
 
-  property :file, 'CompleteName'
+  property :file, 'CompleteName', Pathname
   property :size, 'FileSize', Integer
   property :duration, 'Duration', Integer
   property :container, 'Format'
+  property :mime_type, 'InternetMediaType'
+  property :size, 'FileSize', Integer
+  property :duration, 'Duration', Integer
+  property :bitrate, 'OverallBitRate_Mode', Integer
+  property :interleaved, 'Interleaved', Boolean
 end
