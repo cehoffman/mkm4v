7dae33ebdd1711fdaa175046d787c322dfef2c3c
Chris Hoffman <cehoffman@gmail.com>
2010-03-15 12:55:31 -0500

    Let ruby take care of escaping params properly

--- lib/handbrake.rb
+++ lib/handbrake.rb
@@ -7,7 +7,7 @@ class Handbrake
   def initialize(filename)
     @file = Pathname.new(filename).expand_path
 
-    stdin, stdout, stderr = Open3.popen3 %|#{Exe} -i "#{filename}" --title 0|
+    stdin, stdout, stderr = Open3.popen3 Exe, "-i", filename, "--title", "0"
     stdout = stdout.read
     stderr = stderr.read
 
@@ -17,8 +17,6 @@ class Handbrake
   def title?
     !@title.empty?
   end
-
-private
 end
 
 require File.expand_path("../handbrake/title", __FILE__)
