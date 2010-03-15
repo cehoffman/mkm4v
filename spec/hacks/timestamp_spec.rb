bd26770767ef8204b408b2af9e4ea59236208ccc
Chris Hoffman <cehoffman@gmail.com>
2010-03-15 12:06:40 -0500

    The updated specs for Timestamp class

--- spec/hacks/timestamp_spec.rb
+++ spec/hacks/timestamp_spec.rb
@@ -39,11 +39,58 @@ describe Timestamp do
     stamp.to_s.should == "00:00:56.120"
   end
 
-  it "should be comparable" do
+  it "should be comparable with other Timestamps" do
     earlier, later = Timestamp.new(0), Timestamp.new(33.2)
 
     earlier.should < later
     earlier.should == Timestamp.new(0)
     later.should > earlier
+  end
+
+  it "should be comparable with other Numbers" do
+    earlier, later = Timestamp.new(0), 33.2
+
+    earlier.should < later
+    earlier.should == Timestamp.new(0)
+  end
+
+  it "should be addable with other Timestamps" do
+    stamp = Timestamp.new(30) + Timestamp.new(21)
+
+    stamp.should be_an_instance_of(Timestamp)
+    stamp.seconds.should == 51
+  end
+
+  it "should be addable with other numbers" do
+    stamp = Timestamp.new(30) + 21
+
+    stamp.should be_an_instance_of(Timestamp)
+    stamp.seconds.should == 51
+  end
+
+  it "should be subtractable with other Timestamps" do
+    stamp = Timestamp.new(30) - Timestamp.new(21)
+
+    stamp.should be_an_instance_of(Timestamp)
+    stamp.seconds.should == 9
+  end
+
+  it "should be subtractable with other numbers" do
+    stamp = Timestamp.new(30) - 21
+
+    stamp.should be_an_instance_of(Timestamp)
+    stamp.seconds.should == 9
+  end
+
+  it "should have an accessor for number of hours" do
+    stamp = Timestamp.new("01:30:00")
+
+    stamp.hours.should == 1.5
+  end
+
+  it "should have an accessor for the number of minutes" do
+    stamp = Timestamp.new("01:31:00")
+
+    stamp.minutes.should == 91
   end
 end
