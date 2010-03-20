be958279daa5fb75d1073457903402d20c488c56
Chris Hoffman <cehoffman@gmail.com>
2010-03-20 17:38:42 -0500

    Mp4v2 specs for metadata

--- spec/mp4v2/mp4v2_spec.rb
+++ spec/mp4v2/mp4v2_spec.rb
@@ -14,7 +14,179 @@ describe Mp4v2 do
     FileUtils.rm @file.extname("test")
   end
 
+  it "should have access to metadata through keys" do
+    @mp4.should have_key(:name)
+    @mp4[:name].should == "Pilot"
+  end
+
+  it "should have methods to access keys" do
+    @mp4.should respond_to(:name)
+    @mp4.name.should == "Pilot"
+  end
+
+  it "should set corresponding keys when setting properties from methods" do
+    @mp4.should_not have_key(:album_artist)
+    @mp4.album_artist = "Your Mom"
+    @mp4.should have_key(:album_artist)
+  end
+
+  it "should return nil for properties that are not set" do
+    @mp4.album_artist.should == nil
+  end
+
   it "should know the file it opened" do
     @mp4.file.should =~ /\/mp4v2\.test$/
+  end
+
+  def self.metadata(meta, value, &block)
+    specify "#{meta} should be gettable, settable, and clearable" do
+      @mp4[meta] = value
+      @mp4.save reload: true
+      @mp4[meta].should == value
+      @mp4[meta] = nil
+      @mp4.save reload: true
+      @mp4.should_not have_key(meta)
+    end
+  end
+
+  metadata :name, "A Name"
+  metadata :artist, "Some Artist"
+  metadata :album_artist, "Album Artist"
+  metadata :album, "Album"
+  metadata :grouping, "Grouping"
+  metadata :composer, "Composer"
+  metadata :comments, "Comments"
+  metadata :genre, "Genre"
+  metadata :genre_type, 2**16 - 1
+  metadata :released, DateTime.civil(2004, 11, 16, 6)
+  metadata :track, 2**16 - 1
+  metadata :tracks, 2**16 - 1
+  metadata :disk, 2**16 - 1
+  metadata :disks, 2**16 - 1
+  metadata :tempo, 2**16 - 1
+  metadata :show, "Show"
+  metadata :episode_id, "ID"
+  metadata :season, 2**32 - 1
+  metadata :episode, 2**32 - 1
+  metadata :network, "Network"
+  metadata :description, "A Description"
+  metadata :long_description, "A Long description"
+  metadata :lyrics, "Lyrics"
+  metadata :copyright, "Copyright"
+  metadata :encoding_tool, "HandBrake"
+  metadata :encoded_by, "Me"
+  metadata :category, "Category"
+
+  # media type is a set of possible symbols
+  metadata :kind, :music
+  metadata :kind, :audiobook
+  metadata :kind, :music_video
+  metadata :kind, :movie
+  metadata :kind, :tv
+  metadata :kind, :booklet
+  metadata :kind, :ringtone
+
+  specify "kind should not be sent when invalid" do
+    @mp4.kind = :invalid
+    @mp4.save reload: true
+    @mp4.should_not have_key(:kind)
+  end
+
+  # content rating is a set of possible symbols
+  metadata :advisory, :none
+  metadata :advisory, :clean
+  metadata :advisory, :explicit
+
+  specify "advisory warning should not be set when invalid" do
+    @mp4.advisory = :invalid
+    @mp4.save reload: true
+    @mp4.should_not have_key(:invalid)
+  end
+
+  metadata :purchased, DateTime.civil(2009, 12, 1)
+  metadata :account, "iTunes account"
+  metadata :account_type, 255
+  metadata :country, 2*32 - 1
+  metadata :cnID, 2**32 - 1
+  metadata :atID, 2**32 - 1
+  metadata :plID, 2**64 - 1
+  metadata :geID, 2**32 - 1
+
+  specify "compilation should have a boolean accessor" do
+    @mp4.should_not be_compilation
+    @mp4.compilation = true
+    @mp4.save reload: true
+    @mp4.should be_compilation
+  end
+
+  specify "podcast should have a boolean accessor" do
+    @mp4.should_not be_podcast
+    @mp4.podcast = true
+    @mp4.save reload: true
+    @mp4.should be_podcast
+  end
+
+  specify "hd should have a boolean accessor" do
+    @mp4.should_not be_hd
+    @mp4.hd = true
+    @mp4.save reload: true
+    @mp4.should be_hd
+  end
+
+  specify "gapless should have a boolen accessor" do
+    @mp4.should_not be_gapless
+    @mp4.gapless = true
+    @mp4.save reload: true
+    @mp4.should be_gapless
+  end
+
+  describe "#save" do
+    it "should accept a hash of options" do
+      @mp4.save option: "Value"
+    end
+
+    it "it should return itself after save" do
+      @mp4.save.__id__.should == @mp4.__id__
+    end
+
+    it "should take an option to reload the file" do
+      @mp4.should_receive(:reload!).once
+      @mp4.save reload: true
+    end
+
+    it "should not reload by default" do
+      @mp4.should_not_receive(:reload!)
+      @mp4.save
+    end
+
+    it "should take an option to optimize the file" do
+      @mp4.should_receive(:optimize!).once
+      @mp4.save optimize: true
+    end
+
+    it "should not optimize by default" do
+      @mp4.should_not_receive(:optimize!)
+      @mp4.save
+    end
+
+    it "should optimize and then reload when both options given" do
+      @mp4.should_receive(:optimize!).ordered
+      @mp4.should_receive(:reload!).ordered
+
+      @mp4.save optimize: true, reload: true
+    end
+  end
+
+  describe "#reload!" do
+    it "should drop all fields that are not in file" do
+      @mp4.should_not have_key(:album_artist)
+      @mp4.album_artist = "Your Mom"
+      @mp4.album_artist.should == "Your Mom"
+
+      @mp4.reload!
+
+      @mp4.should_not have_key(:album_artist)
+      @mp4.album_artist.should == nil
+    end
   end
 end
